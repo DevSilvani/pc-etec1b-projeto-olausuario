0e96dd899259a234745115b33dba4aab7670cabc
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-11 18:41:27 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_olausuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá, "+nome+"!");
+            Console.ReadKey();
+        }
+    }
+}
